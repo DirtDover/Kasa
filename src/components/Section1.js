8353c2137c07a590460c51b43764c99f5a19127c
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-03-06 12:40:21 +0100

    Test Section 1

---
+++ src/components/Section1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import'../styles/Section1.css'
+
+const Section1 = () => {
+    return (
+        <div className='section1'>
+            <img src='./Background.png' alt='Background' className='img-section1' />
+            <h1 className='title-section1'>Chez vous, partout et ailleurs</h1>
+            
+        </div>
+    );
+};
+
+export default Section1;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../../components/Banner';
+import Section1 from '../../components/Section1';
 import '../home/Home.css'
 
 
@@ -7,6 +8,7 @@ const Home = () => {
     return (
         <div>
            <Banner />
+           <Section1 />
         </div>
     );
 };
