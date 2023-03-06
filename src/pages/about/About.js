34dd3afe1d604516833445bcb72deebe0a444640
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-03-06 13:08:30 +0100

    Image A Propos

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../../components/Banner';
+import './About.css'
 
 
 const About = () => {
@@ -7,6 +8,8 @@ const About = () => {
         
         <div>
             <Banner />
+            <img src='./about-background.png' alt='A propos' className='img-about' />
+            
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo tempora at nesciunt quo facere magnam vel odio voluptatum porro eius? Quidem adipisci provident numquam, excepturi magnam commodi! Voluptate ipsum, quidem blanditiis debitis, fugiat praesentium impedit expedita, doloribus corporis molestias sunt quibusdam distinctio. Sed odio eius nisi natus! Odio illum velit asperiores neque necessitatibus et accusantium debitis, nobis recusandae quidem eos molestias deserunt fugit magnam at quis ipsam pariatur labore esse similique dicta quo laborum aperiam perferendis. Nam, natus sunt non quia quibusdam temporibus doloribus quasi ut, fugit ea accusantium molestias, officiis tempore eveniet illum minima veritatis pariatur a suscipit necessitatibus?</p>
         </div>
     );
