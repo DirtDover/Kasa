99bfc38ad84efbc4d02d1adf0695adab8be88775
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-03-06 13:31:31 +0100

    Pages d'erreur

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Banner from '../../components/Banner';
+import './Error.css'
 
 const Error = () => {
     return (
         <div>
-           <h1>Oups le lien n'est pas bon</h1> 
+        <Banner />
+        <div className='container-error'>
+        <h1 className='error-404'>404</h1>
+           <h2 className='title-error'>Oups ! La page que vous demandez n'existe pas</h2>
+
+           <a href="/" className='lien-error'>Retourner sur la page d'acceuil</a> 
+        </div>
         </div>
     );
 };
