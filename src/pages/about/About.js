f36e98e0d248633d7f0076c8374d159b0b2c5e4c
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-03-07 11:56:29 +0100

    Carte Cliquable

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../components/Banner';
 import Footer from '../../components/Footer';
 import './About.css'
+import about from '../../assets/about-background.png'
 
 
 const About = () => {
@@ -9,7 +10,7 @@ const About = () => {
         
         <div>
             <Banner />
-            <img src='./about-background.png' alt='A propos' className='img-about' />
+            <img src={about} alt='A propos' className='img-about' />
             
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo tempora at nesciunt quo facere magnam vel odio voluptatum porro eius? Quidem adipisci provident numquam, excepturi magnam commodi! Voluptate ipsum, quidem blanditiis debitis, fugiat praesentium impedit expedita, doloribus corporis molestias sunt quibusdam distinctio. Sed odio eius nisi natus! Odio illum velit asperiores neque necessitatibus et accusantium debitis, nobis recusandae quidem eos molestias deserunt fugit magnam at quis ipsam pariatur labore esse similique dicta quo laborum aperiam perferendis. Nam, natus sunt non quia quibusdam temporibus doloribus quasi ut, fugit ea accusantium molestias, officiis tempore eveniet illum minima veritatis pariatur a suscipit necessitatibus?</p>
             <Footer />
