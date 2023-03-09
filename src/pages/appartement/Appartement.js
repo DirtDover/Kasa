5f283f492796e5c2d6c107a8e8833936c2d7ed77
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-03-09 11:53:05 +0100

    Start Carroussel

--- src/pages/appartement/Appartement.js
+++ src/pages/appartement/Appartement.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Banner from '../../components/Banner';
 import Footer from '../../components/Footer';
+import datas from '../../datas/appartList';
+import Slider from '../../components/Slider'
 
-const appartement = () => {
+const Appartement = () => {
+
+const[imageSlider, setImageSlider] = useState([])
+
+const idAppart = useParams('id').id;
+const currentAppart = datas.filter(data => data.id === idAppart)
+
+useEffect(()=> {
+    const currentAppart = datas.filter(data => data.id === idAppart);
+    setImageSlider(currentAppart[0].pictures);
+},[idAppart])
+
+
+
+
+
     return (
         <div>
             <Banner />
-            
+            <Slider imageSlider={imageSlider} />
             <Footer />
             
         </div>
     );
 };
 
-export default appartement;
+export default Appartement;
