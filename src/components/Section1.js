213889d73d0af6963364ce91ccba359c29428bdb
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-03-27 14:21:03 +0200

    Update Section 1

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -6,7 +6,7 @@ const Section1 = () => {
     return (
         <div className='section1'>
             <img src={background} alt='Background' className='img-section1' />
-            <h1 className='title-section1'>Chez vous, partout et ailleurs</h1>
+            <h1 className='title-section1'>Chez vous,<br className='br'/> partout et ailleurs</h1>
             
         </div>
     );
