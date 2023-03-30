64deea106f483c8b0542eb1ca45286c7ebb47f27
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-03-30 17:41:10 +0200

    Router fichier a part

---
+++ src/Route.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import About from './pages/about/About';
+import Error from './pages/error/Error';
+import Home from './pages/home/Home';
+import Appartement from './pages/appartement/Appartement';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route exact path='/' element={<Home />}></Route>
+      <Route exact path='/about' element={<About />}></Route>
+      <Route path='*' element={<Error />}></Route>
+      <Route path='/Appartement/:id' element={<Appartement />}></Route>
+      </Routes>    
+    </BrowserRouter>
+  );
+};
+
+export default Router;
